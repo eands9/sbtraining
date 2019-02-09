88581eb40ddcb612aa70d635ff2179f51edbbae1
Eric Hernandez <eands9@yahoo.com>
2019-02-09 12:50:50 -0600

    Initial Commit

---
+++ sbtraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sbtraining
+//
+//  Created by Eric Hernandez on 2/9/19.
+//  Copyright © 2019 Eric Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
